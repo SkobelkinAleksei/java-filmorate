92f409944cba456d063167b3a2a32b5d0bb99e4e
Скобелкин Алексей <skobelkin.97@inbox.ru>
2025-02-24 20:36:44 +0700

    /

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
@@ -29,7 +30,7 @@ public class UserController {
 
     // Получение User
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable long id) {
+    public User getUserById(@PathVariable long id) throws NotFoundException {
         return userService.getUser(id);
     }
 
@@ -42,13 +43,13 @@ public class UserController {
 
     // Обновляем данные Пользователя
     @PutMapping
-    public User update(@Valid @RequestBody User newUser) {
+    public User update(@Valid @RequestBody User newUser) throws NotFoundException {
         return userService.update(newUser);
     }
 
     // Добавление в друзья
     @PutMapping("/{userId}/friends/{friendId}")
-    public void addFriend(@PathVariable long userId, @PathVariable long friendId) {
+    public void addFriend(@PathVariable long userId, @PathVariable long friendId) throws NotFoundException {
         userService.addFriend(userId, friendId);
     }
 
@@ -61,13 +62,13 @@ public class UserController {
 
     // Возвращаем друзей User
     @GetMapping("/{userId}/friends")
-    public Set<User> getFriends(@PathVariable long userId) {
+    public Set<User> getFriends(@PathVariable long userId) throws NotFoundException {
         return userService.getFriends(userId);
     }
 
     // Список общих друзей
     @GetMapping("/{user1}/friends/common/{user2}")
-    public List<User> getMutualFriends(@PathVariable Long user1, @PathVariable Long user2) {
+    public List<User> getMutualFriends(@PathVariable Long user1, @PathVariable Long user2) throws NotFoundException {
         return userService.getMutualFriends(user1, user2);
     }
 }
