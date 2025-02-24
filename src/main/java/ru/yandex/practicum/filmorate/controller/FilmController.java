5696e7c1969b6f7b9a19e1b39af3f9ad52b1e05d
Скобелкин Алексей <skobelkin.97@inbox.ru>
2025-02-24 20:38:42 +0700

    /

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
@@ -25,7 +26,7 @@ public class FilmController {
 
     // Получение фильма
     @GetMapping("/{id}")
-    public Film getFilm(@PathVariable long id) {
+    public Film getFilm(@PathVariable long id) throws NotFoundException {
         return filmService.getFilm(id);
     }
 
@@ -38,26 +39,26 @@ public class FilmController {
 
     // Обновляем данные фильма
     @PutMapping
-    public Film update(@Valid @RequestBody Film newFilm) {
+    public Film update(@Valid @RequestBody Film newFilm) throws NotFoundException {
         return filmService.update(newFilm);
     }
 
     // Ставим лайк фильму
     @PutMapping("/{filmId}/like/{userId}")
-    public void addLike(@PathVariable Long filmId, @PathVariable Long userId) {
+    public void addLike(@PathVariable Long filmId, @PathVariable Long userId) throws NotFoundException {
         filmService.addLike(filmId, userId);
     }
 
     //  пользователь удаляет лайк
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{filmId}/like/{userId}")
-    public void removeLike(@PathVariable Long filmId, @PathVariable Long userId) {
+    public void removeLike(@PathVariable Long filmId, @PathVariable Long userId) throws NotFoundException {
         filmService.removeLike(filmId, userId);
     }
 
     // Возвращает список из первых 10-и
     @GetMapping("/popular")
-    public Collection<Film> getTopMovies() {
+    public Collection<Film> getTopMovies() throws NotFoundException {
         return filmService.getTopMovies();
     }
 }
