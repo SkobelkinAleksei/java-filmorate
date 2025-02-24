9016d388331f25f4e232cd56e55030b30713e4e0
Скобелкин Алексей <skobelkin.97@inbox.ru>
2025-02-24 21:25:43 +0700

    Тест

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/error/ErrorHandler.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.exception.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationError(final ValidationException e) {
+        log.info("ERROR[400]: Произошла ошибка ValidationException: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundError(final NotFoundException e) {
+        log.info("ERROR[404]: Произошла ошибка NotFoundException: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse exceptionError(final Exception e) {
+        log.info("ERROR[500]: Произошла ошибка ErrorException: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+}
